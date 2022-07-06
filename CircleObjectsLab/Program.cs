b98d77add0d35f274c73461f8f4d4397f871f4eb
Travis <travisamador@gmail.com>
2022-07-06 08:26:43 -0400

    second commit, removed unneccessary loop

--- CircleObjectsLab/Program.cs
+++ CircleObjectsLab/Program.cs
@@ -1,25 +1,17 @@
 ﻿using CircleObjectsLab;
+Console.WriteLine("Welcome to the Circle Tester.");
 bool runProgram = true;
 while (runProgram)
-{
-    Console.WriteLine("Welcome to the Circle Tester.");
+{  
     Console.WriteLine("Please enter the radius of a circle: ");
     Circle userCircle = new Circle(Validator.Double());
     Console.WriteLine($"The circumference of your circle is: {userCircle.CalculateFormattedCircumference()}");
     Console.WriteLine($"The area of your circle is: {userCircle.CalculateFormattedArea()}");
     Console.WriteLine("Would you like to test another circle? (y/n)");
-    while (true)
+    string answer = Validator.YesOrNo();
+    if (answer == "n")
     {
-        string answer = Validator.YesOrNo();
-        if (answer == "y")
-        {
-            break;
-        }
-        else if (answer == "n")
-        {
-            Console.WriteLine($"Goodbye, you made {Circle.circleCount} circle(s).");
-            runProgram = false;
-            break;
-        }
-    }
+        Console.WriteLine($"Goodbye, you made {Circle.circleCount} circle(s).");
+        runProgram = false;
+    }   
 }
